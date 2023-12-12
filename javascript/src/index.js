2d13e7f88b25b9dfdfd279978d3ea6e73cabaf51
= <harmtrav@gmail.com>
2023-12-12 10:44:53 -0600

    Update version

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,5 +1,5 @@
-import { mount, rendr } from '@rendrjs/core';
+import { mount } from '@rendrjs/core';
 import App from './App';
 import './index.css';
 
-mount(document.querySelector('#root'), rendr(App));
+mount(document.querySelector('#root'), App);
