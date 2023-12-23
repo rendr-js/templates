c4b1a0ac324234daba9c7d366be83de3a660ba82
Travis Harmon <harmtrav@gmail.com>
2023-12-23 12:07:41 -0600

    Update index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,5 +1,5 @@
-import { mount } from '@rendrjs/core';
+import { mount, component } from '@rendrjs/core';
 import App from './App.js';
 import './index.css';
 
-mount(document.querySelector('#root')!, App);
+mount(document.querySelector('#root')!, component(App));
