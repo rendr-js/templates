9892994f6d0f8751e9f7c011cd498635b8f54c82
= <harmtrav@gmail.com>
2023-12-30 13:02:55 -0600

    Update testing-library version

--- javascript/src/App.test.js
+++ javascript/src/App.test.js
@@ -1,11 +1,11 @@
-import { rendr } from '@rendrjs/core';
+import { component } from '@rendrjs/core';
 import { mount, userEvent, screen } from '@rendrjs/testing-library';
 import { expect, test } from 'vitest';
 import App from './App.js';
 
 test('counts', async () => {
     const user = userEvent.setup();
-    const container = mount(rendr(App));
+    const container = mount(component(App));
     const btn = screen.getByText(container, /^count/);
     expect(btn.textContent).toBe('count: 0');
     user.click(btn);
